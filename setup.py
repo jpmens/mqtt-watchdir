c903b34d971e52a00f39b7b83dc1f39c22edd274
Jan-Piet Mens <jpmens@gmail.com>
2013-08-15 21:37:10 +0200

    PyPi-able

--- setup.py
+++ setup.py
@@ -8,13 +8,30 @@ except ImportError:
 
 setup(
     name='mqtt-watchdir',
+    version='1.2',
+    author='Jan-Piet Mens',
+    author_email='jpmens@gmail.com',
     url="https://github.com/jpmens/mqtt-watchdir",
-    long_description=__doc__,
+    description='Recursively watch a directory for modifications and publish file content to an MQTT broker',
+    license='BSD License',
     scripts=[
         "mqtt-watchdir.py"
     ],
     install_requires=[
         'watchdog',
         'mosquitto',
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications',
+        'Topic :: Internet',
+        ]
+
 )
