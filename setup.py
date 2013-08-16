c85af09c22fc61aa0a42494e2ad2095ea5b915f1
Jan-Piet Mens <jpmens@gmail.com>
2013-08-16 12:27:43 +0200

    add keywords and karlp's git_version magic

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
 from setuptools import setup
 
+# from https://gist.github.com/dcreager/300803 with "-dirty" support added
+from version import get_git_version
+
 # From http://bugs.python.org/issue15881
 try:
     import multiprocessing
 except ImportError:
     pass
 
-with open('readme.rst') as file:
+long_description = ''
+
+with open('README.rst') as file:
     long_description = file.read()
 
 setup(
     name='mqtt-watchdir',
-    version='1.3',
+    version=get_git_version(),
     author='Jan-Piet Mens',
     author_email='jpmens@gmail.com',
     url="https://github.com/jpmens/mqtt-watchdir",
     description='Recursively watch a directory for modifications and publish file content to an MQTT broker',
     license='BSD License',
     long_description=long_description,
+    keywords = [
+        "MQTT",
+        "files",
+        "notify"
+    ],
     scripts=[
         "mqtt-watchdir.py"
+    ],
+    data_files=[
+        "README.rst"
     ],
     install_requires=[
         'watchdog',
